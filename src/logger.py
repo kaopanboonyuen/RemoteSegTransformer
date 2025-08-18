5e6a88134c3fa3d40b8973aee387cdcc2e63e8e3
Kao Panboonyuen <panboonyuen.kao@gmail.com>
2025-08-18 17:58:50 +0700

    🐳 update create_logger to logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,8 @@
 # --------------------------------------------------------
-# Swin Transformer
+# Swin Transformer Logger Utility (Refactored)
 # Copyright (c) 2021 Microsoft
 # Licensed under The MIT License [see LICENSE for details]
-# Written by Ze Liu
+# Refactored by Teerapong Panboonyuen (Kao)
 # --------------------------------------------------------
 
 import os
@@ -11,31 +11,43 @@ import logging
 import functools
 from termcolor import colored
 
-
 @functools.lru_cache()
-def create_logger(output_dir, dist_rank=0, name=''):
-    # create logger
+def create_logger(output_dir: str, dist_rank: int = 0, name: str = ''):
+    """
+    Creates and configures a logger instance with both console and file output.
+
+    Args:
+        output_dir (str): Directory where log file will be saved.
+        dist_rank (int): Distributed rank (used to control logging for master process).
+        name (str): Logger name.
+
+    Returns:
+        logging.Logger: Configured logger instance.
+    """
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     logger.propagate = False
 
-    # create formatter
-    fmt = '[%(asctime)s %(name)s] (%(filename)s %(lineno)d): %(levelname)s %(message)s'
-    color_fmt = colored('[%(asctime)s %(name)s]', 'green') + \
-                colored('(%(filename)s %(lineno)d)', 'yellow') + ': %(levelname)s %(message)s'
+    # Log format definitions
+    base_format = '[%(asctime)s %(name)s] (%(filename)s %(lineno)d): %(levelname)s %(message)s'
+    color_format = (
+        colored('[%(asctime)s %(name)s]', 'green') +
+        colored(' (%(filename)s:%(lineno)d)', 'yellow') +
+        ': %(levelname)s %(message)s'
+    )
 
-    # create console handlers for master process
+    # Console logger for master process
     if dist_rank == 0:
         console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setLevel(logging.DEBUG)
-        console_handler.setFormatter(
-            logging.Formatter(fmt=color_fmt, datefmt='%Y-%m-%d %H:%M:%S'))
+        console_handler.setFormatter(logging.Formatter(fmt=color_format, datefmt='%Y-%m-%d %H:%M:%S'))
         logger.addHandler(console_handler)
 
-    # create file handlers
-    file_handler = logging.FileHandler(os.path.join(output_dir, f'log_rank{dist_rank}.txt'), mode='a')
+    # File logger for all ranks
+    log_file = os.path.join(output_dir, f'log_rank{dist_rank}.txt')
+    file_handler = logging.FileHandler(log_file, mode='a')
     file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(logging.Formatter(fmt=fmt, datefmt='%Y-%m-%d %H:%M:%S'))
+    file_handler.setFormatter(logging.Formatter(fmt=base_format, datefmt='%Y-%m-%d %H:%M:%S'))
     logger.addHandler(file_handler)
 
     return logger
